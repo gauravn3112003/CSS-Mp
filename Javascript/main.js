ff9c231f656eef9d0071db3fcdf92af5d7e5bcc7
Gaurav Narnaware <gauravnarnaaware3112003@gmail.com>
2022-11-13 14:56:13 +0530

    initial commit

---
+++ Javascript/main.js
@@ -0,0 +1,12 @@
+const toggleMenuOpen = () => document.body.classList.toggle("open");
+// const toggleExpand = () => document.body.classList.toggle("view");
+
+
+// const toggleExpand = () =>{
+//     var expand = document.getElementById("IMPLink")
+//     expand.classList.toggle("view")
+// }
+// const toggleDeptExpand = () =>{
+//     var expand = document.getElementById("IMPDepartment")
+//     expand.classList.toggle("view")
+// }
